516c79506bcb54e3290928084df64427554e1218
adsonatural <adsonhenriquesilva@gmail.com>
2021-05-27 16:44:34 +0200

    add new imports

--- dogbook/src/app/home/home.module.ts
+++ dogbook/src/app/home/home.module.ts
@@ -1,13 +1,15 @@
+import { MessageModule } from './../components/message/message.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, MessageModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
