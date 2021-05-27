a3e2136a665cdb2384416dba4e74e96c1d76074b
adsonatural <adsonhenriquesilva@gmail.com>
2021-05-27 16:45:10 +0200

    add method login who calls authentication api

--- dogbook/src/app/home/login/login.component.ts
+++ dogbook/src/app/home/login/login.component.ts
@@ -1,15 +1,31 @@
+import { AuthenticationService } from './../../authentication/authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  user = '';
+  password = '';
 
-  constructor() { }
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login(): void {
+    this.authService.authenticate(this.user, this.password).subscribe(
+      () => {
+        this.router.navigate(['animals']);
+      },
+      (_) => {
+        alert('Invalid(s) user and/or password');
+      }
+    );
   }
-
 }
