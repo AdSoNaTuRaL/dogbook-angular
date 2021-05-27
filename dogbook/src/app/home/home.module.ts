5545879f47bb3b4e334c26d23fb02041b59bdbac
adsonatural <adsonhenriquesilva@gmail.com>
2021-05-27 17:24:01 +0200

    add reactiveformsmodule

--- dogbook/src/app/home/home.module.ts
+++ dogbook/src/app/home/home.module.ts
@@ -5,11 +5,18 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NewUserComponent } from './new-user/new-user.component';
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
-  imports: [CommonModule, HomeRoutingModule, FormsModule, MessageModule],
+  declarations: [HomeComponent, LoginComponent, NewUserComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    FormsModule,
+    MessageModule,
+    ReactiveFormsModule,
+  ],
   exports: [HomeComponent],
 })
 export class HomeModule {}
