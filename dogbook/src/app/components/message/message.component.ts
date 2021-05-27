81bf665068272a0e5c93a08ab096715bde9f4b2a
adsonatural <adsonhenriquesilva@gmail.com>
2021-05-27 16:44:09 +0200

    create a message module and component

---
+++ dogbook/src/app/components/message/message.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.css'],
+})
+export class MessageComponent implements OnInit {
+  @Input()
+  messageText = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
