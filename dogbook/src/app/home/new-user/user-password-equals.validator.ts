2dce9227f6daf87945c6cc2d02acec0f2d3972a0
adsonatural <adsonhenriquesilva@gmail.com>
2021-05-28 12:41:53 +0200

    create a validator for check if user and password are equals

---
+++ dogbook/src/app/home/new-user/user-password-equals.validator.ts
@@ -0,0 +1,12 @@
+import { AbstractControl } from '@angular/forms';
+
+export function userPasswordEqualsValidator(formGroup: AbstractControl): object | null {
+  const username = formGroup.get('userName')?.value ?? '';
+  const password = formGroup.get('password')?.value ?? '';
+
+  if (username.trim() + password.trim()) {
+    return username !== password ? null : { userEqualsPassword: true };
+  } else {
+    return null;
+  }
+}
