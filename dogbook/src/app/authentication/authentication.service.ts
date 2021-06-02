bcad5c091cd10fbda31a669489111095ca1f687f
adsonatural <adsonhenriquesilva@gmail.com>
2021-06-02 17:17:20 +0200

    change authenticate service for store user token when login

--- dogbook/src/app/authentication/authentication.service.ts
+++ dogbook/src/app/authentication/authentication.service.ts
@@ -1,18 +1,34 @@
+import { UserService } from './user/user.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private userService: UserService
+  ) {}
 
-  authenticate(user: string, password: string): Observable<any> {
-    return this.httpClient.post(`${environment.apiUrl}/user/login`, {
-      userName: user,
-      password,
-    });
+  authenticate(user: string, password: string): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        `${environment.apiUrl}/user/login`,
+        {
+          userName: user,
+          password,
+        },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-acess-token') ?? '';
+          this.userService.storeUserToken(authToken);
+        })
+      );
   }
 }
