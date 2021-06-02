58ae25e259f48190d4da7deb813a7917b95c37f8
adsonatural <adsonhenriquesilva@gmail.com>
2021-06-02 17:49:02 +0200

    import header and footer module on main module

--- dogbook/src/app/app.module.ts
+++ dogbook/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { FooterModule } from './components/footer/footer.module';
+import { HeaderModule } from './components/header/header.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,7 +9,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HeaderModule,
+    FooterModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
