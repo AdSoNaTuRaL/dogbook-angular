928e1d292a846dbef3811805d5957407a5b75c65
adsonatural <adsonhenriquesilva@gmail.com>
2021-06-30 16:16:23 +0200

    create animal component

---
+++ dogbook/src/app/animals/animal/animal.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-animal',
+  templateUrl: './animal.component.html',
+  styleUrls: ['./animal.component.css'],
+})
+export class AnimalComponent implements OnInit {
+  private urlOriginal = '';
+
+  @Input() description = '';
+
+  @Input() set url(url: string) {
+    if (url.startsWith('data')) {
+      this.urlOriginal = url;
+    } else {
+      this.urlOriginal = `${environment.apiUrl}/imgs/${url}`;
+    }
+  }
+
+  get url(): string {
+    return this.urlOriginal;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
