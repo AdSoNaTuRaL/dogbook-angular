54f18809da18a63cfb26acb1a300a88b98ee9d90
adsonatural <adsonhenriquesilva@gmail.com>
2021-07-01 16:45:48 +0200

    fix bug

--- dogbook/src/app/animals/animals.module.ts
+++ dogbook/src/app/animals/animals.module.ts
@@ -5,10 +5,9 @@ import { AnimalsRoutingModule } from './animals-routing.module';
 import { ListAnimalsComponent } from './list-animals/list-animals.component';
 import { AnimalComponent } from './animal/animal.component';
 import { CardModule } from '../components/card/card.module';
-import { GridAnimalsPicturesComponent } from './grid-animals-pictures/grid-animals-pictures.component';
 
 @NgModule({
-  declarations: [ListAnimalsComponent, AnimalComponent, GridAnimalsPicturesComponent],
+  declarations: [ListAnimalsComponent, AnimalComponent],
   imports: [CommonModule, AnimalsRoutingModule, CardModule],
 })
 export class AnimalsModule {}
