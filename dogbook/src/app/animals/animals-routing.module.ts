7bae4a1afec6c26ec8d0a43bed5608f84d64d569
adsonatural <adsonhenriquesilva@gmail.com>
2021-07-01 16:45:39 +0200

    format code

--- dogbook/src/app/animals/animals-routing.module.ts
+++ dogbook/src/app/animals/animals-routing.module.ts
@@ -6,11 +6,11 @@ const routes: Routes = [
   {
     path: '',
     component: ListAnimalsComponent,
-  }
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AnimalsRoutingModule { }
+export class AnimalsRoutingModule {}
