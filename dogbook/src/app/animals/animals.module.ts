08c7c88e67244b28540f8fec94b2f54f90250ed3
adsonatural <adsonhenriquesilva@gmail.com>
2021-07-02 15:46:15 +0200

    import grid animals pictures

--- dogbook/src/app/animals/animals.module.ts
+++ dogbook/src/app/animals/animals.module.ts
@@ -5,9 +5,10 @@ import { AnimalsRoutingModule } from './animals-routing.module';
 import { ListAnimalsComponent } from './list-animals/list-animals.component';
 import { AnimalComponent } from './animal/animal.component';
 import { CardModule } from '../components/card/card.module';
+import { GridAnimalsPicturesComponent } from './grid-animals-pictures/grid-animals-pictures.component';
 
 @NgModule({
-  declarations: [ListAnimalsComponent, AnimalComponent],
+  declarations: [ListAnimalsComponent, AnimalComponent, GridAnimalsPicturesComponent],
   imports: [CommonModule, AnimalsRoutingModule, CardModule],
 })
 export class AnimalsModule {}
