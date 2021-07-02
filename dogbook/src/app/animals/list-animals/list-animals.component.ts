732cd863a539d85f790c1c793f5712ed52cf8b2f
adsonatural <adsonhenriquesilva@gmail.com>
2021-07-02 15:46:39 +0200

    refactor

--- dogbook/src/app/animals/list-animals/list-animals.component.ts
+++ dogbook/src/app/animals/list-animals/list-animals.component.ts
@@ -1,11 +1,28 @@
+import { switchMap } from 'rxjs/operators';
+import { AnimalsService } from './../animals.service';
+import { UserService } from './../../authentication/user/user.service';
+import { Animals } from './../animals';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-animals',
   templateUrl: './list-animals.component.html',
 })
 export class ListAnimalsComponent implements OnInit {
-  constructor() {}
+  animals$!: Observable<Animals>;
 
-  ngOnInit(): void {}
+  constructor(
+    private userService: UserService,
+    private animalsService: AnimalsService
+  ) {}
+
+  ngOnInit(): void {
+    this.animals$ = this.userService.returnUser().pipe(
+      switchMap((user) => {
+        const username = user.name ?? '';
+        return this.animalsService.userList(username);
+      })
+    );
+  }
 }
