5c815d030b42523f99dec28e3544cb07452f95b9
adsonatural <adsonhenriquesilva@gmail.com>
2021-07-05 14:14:27 +0200

    create a route to animal detail

--- dogbook/src/app/animals/animals-routing.module.ts
+++ dogbook/src/app/animals/animals-routing.module.ts
@@ -1,3 +1,4 @@
+import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 import { ListAnimalsComponent } from './list-animals/list-animals.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: ListAnimalsComponent,
+  },
+  {
+    path: ':animalId',
+    component: AnimalDetailComponent,
   },
 ];
 
