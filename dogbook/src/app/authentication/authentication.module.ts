399c45fef808340bc88f48c63ade628ba3be288e
adsonatural <adsonhenriquesilva@gmail.com>
2021-07-05 14:15:47 +0200

    register authentication interceptor

--- dogbook/src/app/authentication/authentication.module.ts
+++ dogbook/src/app/authentication/authentication.module.ts
@@ -1,8 +1,17 @@
+import { AuthenticationInterceptor } from './authentication.interceptor';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthenticationInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class AuthenticationModule {}
