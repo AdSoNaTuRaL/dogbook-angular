f2dc4c365f116ce490567a573c7e08b62278de44
adsonatural <adsonhenriquesilva@gmail.com>
2021-07-19 17:37:32 +0200

    create new animal service

--- dogbook/src/app/animals/animals.service.ts
+++ dogbook/src/app/animals/animals.service.ts
@@ -1,6 +1,5 @@
-import { TokenService } from './../authentication/token.service';
 import { environment } from './../../environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
 import { Animal, Animals } from './animals';
@@ -10,10 +9,7 @@ import { catchError, mapTo } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class AnimalsService {
-  constructor(
-    private httpClient: HttpClient,
-    private tokenService: TokenService
-  ) {}
+  constructor(private httpClient: HttpClient) {}
 
   userList(username: string): Observable<Animals> {
     return this.httpClient.get<Animals>(
@@ -42,5 +38,22 @@ export class AnimalsService {
           return error.status === '304' ? of(false) : throwError(error);
         })
       );
+  }
+
+  upload(
+    description: string,
+    allowComments: boolean,
+    file: File
+  ): Observable<any> {
+    const formData = new FormData();
+    formData.append('description', description);
+    formData.append('allowComments', allowComments ? 'true' : 'false');
+    formData.append('file', file);
+
+    return this.httpClient.post(
+      `${environment.apiUrl}/photos/upload`,
+      formData,
+      { observe: 'events', reportProgress: true }
+    );
   }
 }
